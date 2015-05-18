61365dfc460016b93644819fa98da646e356d8b4
Piotr Kosmowski <kospiotr@gmail.com>
2015-05-18 18:57:07 +0200

    Create new exercise solution

--- src/main/java/com/github/kospiotr/springcore/AppConfig.java
+++ src/main/java/com/github/kospiotr/springcore/AppConfig.java
@@ -6,6 +6,7 @@ import com.github.kospiotr.springcore.scoring.AgeScoringRule;
 import com.github.kospiotr.springcore.scoring.JobScoringRule;
 import com.github.kospiotr.springcore.scoring.RememberingLastScoreRule;
 import com.github.kospiotr.springcore.scoring.ScoringRule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,8 +24,9 @@ public class AppConfig {
 	}
 
 	@Bean
-	public ScoreCalculator enCalculator() {
-		return new ScoreCalculator(new EnglishFraudDetector(), scoringRules(), userScoreRegistry());
+	@Autowired
+	public ScoreCalculator enCalculator(UserScoreRegistry userScoreRegistry) {
+		return new ScoreCalculator(new EnglishFraudDetector(), scoringRules(), userScoreRegistry);
 	}
 
 	@Bean
