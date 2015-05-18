0f0bf16f7bc8a9576cb2fb4f53736b1cc841956e
Piotr Kosmowski <kospiotr@gmail.com>
2015-05-18 16:43:08 +0200

    Create new exercise solution

--- src/main/java/com/github/kospiotr/springcore/App.java
+++ src/main/java/com/github/kospiotr/springcore/App.java
@@ -2,13 +2,19 @@ package com.github.kospiotr.springcore;
 
 import com.github.kospiotr.springcore.model.Loan;
 import com.github.kospiotr.springcore.model.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 		Loan loan = new Loan(1000, User.youngRichUser());
 
-		System.out.println("loan = " + loan);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+		ScoreCalculator calculator = ctx.getBean("calculator", ScoreCalculator.class);
+		System.out.println("score = " + calculator.getScore(loan));
+		System.out.println("score = " + calculator.getScore(loan));
+		System.out.println("score = " + calculator.getScore(loan));
 
 	}
 }
