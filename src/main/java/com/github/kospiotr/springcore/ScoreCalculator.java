f8dd4cd997b9f3156ed796ee5529948b46abf8b2
Piotr Kosmowski <kospiotr@gmail.com>
2015-05-18 15:12:52 +0200

    Update

--- src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
+++ src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
@@ -10,28 +10,43 @@ public class ScoreCalculator {
 
 	private FraudDetector fraudDetector;
 	private List<ScoringRule> scoringRules;
-	private UserScoreRegistry userScoreRegistry;
+
+	public ScoreCalculator() {
+		System.out.println("Constructing ScoreCalculator");
+	}
+
+	public ScoreCalculator(FraudDetector fraudDetector, List<ScoringRule> scoringRules) {
+		System.out.println("Constructing ScoreCalculator with all arguments");
+		this.fraudDetector = fraudDetector;
+		this.scoringRules = scoringRules;
+	}
 
 	public Integer getScore(Loan loan) {
+		System.out.println("getScore for: " + loan);
 		System.out.println("Using fraudDetector: " + fraudDetector);
-		System.out.println("Using userScoreRegistry: " + userScoreRegistry);
 		Integer score = scoringRules
 				.stream()
 				.mapToInt(rule -> rule.getScore(loan))
 				.sum();
-		userScoreRegistry.setLastScoreForUser(loan.getUser(), score);
 		return score;
 	}
 
+	public FraudDetector getFraudDetector() {
+		return fraudDetector;
+	}
+
 	public void setFraudDetector(FraudDetector fraudDetector) {
+		System.out.println("Injecting fraudDetector=" + fraudDetector);
 		this.fraudDetector = fraudDetector;
 	}
 
+	public List<ScoringRule> getScoringRules() {
+		return scoringRules;
+	}
+
 	public void setScoringRules(List<ScoringRule> scoringRules) {
+		System.out.println("Injecting scoringRules=" + scoringRules);
 		this.scoringRules = scoringRules;
 	}
 
-	public void setUserScoreRegistry(UserScoreRegistry userScoreRegistry) {
-		this.userScoreRegistry = userScoreRegistry;
-	}
 }
---
+++ src/main/java/com/github/kospiotr/springcore/scoring/LoansHistoryScoringRule.java
@@ -0,0 +1,13 @@
+package com.github.kospiotr.springcore.scoring;
+
+import com.github.kospiotr.springcore.model.Loan;
+
+public class LoansHistoryScoringRule implements ScoringRule {
+
+	int previousScore = 0;
+
+	@Override
+	public Integer getScore(Loan loan) {
+		return previousScore += 10;
+	}
+}
