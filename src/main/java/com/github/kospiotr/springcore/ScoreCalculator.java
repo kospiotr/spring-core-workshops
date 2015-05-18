a3e921dbe919d1cd96b8e8882c520eb7c7213ec5
Piotr Kosmowski <kospiotr@gmail.com>
2015-05-18 17:23:01 +0200

    Create new exercise solution

--- src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
+++ src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
@@ -12,19 +12,17 @@ import java.util.List;
 @Component
 public class ScoreCalculator {
 
-	@Qualifier("polishFraudDetector")
-	@Autowired
+
 	private FraudDetector fraudDetector;
-	@Autowired
 	private List<ScoringRule> scoringRules;
-	@Autowired
 	private UserScoreRegistry userScoreRegistry;
 
 	public ScoreCalculator() {
 		System.out.println("Constructing ScoreCalculator");
 	}
 
-	public ScoreCalculator(FraudDetector fraudDetector, List<ScoringRule> scoringRules, UserScoreRegistry userScoreRegistry) {
+	@Autowired
+	public ScoreCalculator(@Qualifier("polishFraudDetector") FraudDetector fraudDetector, List<ScoringRule> scoringRules, UserScoreRegistry userScoreRegistry) {
 		System.out.println("Constructing ScoreCalculator with all arguments");
 		this.fraudDetector = fraudDetector;
 		this.scoringRules = scoringRules;
