aa80676c42f1574e00828cdc00b4508e6cf989ca
Piotr Kosmowski <kospiotr@gmail.com>
2015-05-25 23:15:28 +0200

    Workshop update

--- src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
+++ src/main/java/com/github/kospiotr/springcore/ScoreCalculator.java
@@ -1,25 +1,16 @@
 package com.github.kospiotr.springcore;
 
 import com.github.kospiotr.springcore.fraud.FraudDetector;
-import com.github.kospiotr.springcore.fraud.PolishFraudDetector;
 import com.github.kospiotr.springcore.model.Loan;
-import com.github.kospiotr.springcore.scoring.AgeScoringRule;
-import com.github.kospiotr.springcore.scoring.JobScoringRule;
 import com.github.kospiotr.springcore.scoring.ScoringRule;
 
 import java.util.List;
-
-import static java.util.Arrays.asList;
 
 public class ScoreCalculator {
 
 	private FraudDetector fraudDetector;
 	private List<ScoringRule> scoringRules;
 	private UserScoreRegistry userScoreRegistry;
-
-	public static ScoreCalculator createInstance() {
-		return new ScoreCalculator(new PolishFraudDetector(), asList(new AgeScoringRule(), new JobScoringRule()));
-	}
 
 	public ScoreCalculator() {
 		System.out.println("Constructing ScoreCalculator");
